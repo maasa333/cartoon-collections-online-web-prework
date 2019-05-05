ad2e7ca4ba7d09fc364808ab88790fb363b2bdf6
Maasa Kono <konomaasa@gmail.com>
2019-05-05 22:25:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ end
 def summon_captain_planet(planeteer_calls)
   planeteer_calls = []
   array.collect do | elements |
-    planeteer_calls << "#{elements.capitalize + !}"
+    planeteer_calls << "#{elements.capitalize!}"
   end
   planeteer_calls
 end
