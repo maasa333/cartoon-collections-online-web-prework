a3faad78b42601728fd828a5ef3bd3c46122c02e
Maasa Kono <konomaasa@gmail.com>
2019-05-05 22:12:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,29 +1,24 @@
-def roll_call_dwarves(dwarfs)
-  dwarfs.each_with_index do | name , index |
-    puts "#{index + 1} #{name}"
-  end
+def roll_call_dwarves(array)
+  index = 0
+  array.each_with_index {|name, index| puts "#{index + 1}. #{name} "}
 end
 
-def summon_captain_planet(veggies)
-    veggies.collect do |call|
-    call.capitalize + "!"
+def summon_captain_planet(array)
+  planeteer_calls = []
+  array.collect do |calls|
+    planeteer_calls << "#{calls.capitalize}!"
   end
-
+  planeteer_calls
 end
 
-def long_planteer_calls(long_planteer_calls
-  answer = false
-  long_planteer_calls.each do |call|
-    if call.length > 4
-      answer = true
-    end
-  end
-    answer
+def long_planeteer_calls(array)
+  array.any? do |word|
+    word.length > 4
+end
 end
 
-def find_the_cheese(cheese)
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese.find do |cheese|
-    cheese_types.include?(cheese)
+def find_the_cheese(array)
+  array.find do |cheese|
+    cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
   end
 end
