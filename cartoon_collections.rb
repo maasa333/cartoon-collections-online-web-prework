1b53990dee1464eea9dc4f3b7cfaca46000c2c75
Maasa Kono <konomaasa@gmail.com>
2019-05-05 22:24:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,12 +12,3 @@ def summon_captain_planet(planeteer_calls)
   end
   planeteer_calls
 end
-
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
