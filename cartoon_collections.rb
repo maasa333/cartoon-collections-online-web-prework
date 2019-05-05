4163585306571353e531d5f0fa888b2b8d472513
Maasa Kono <konomaasa@gmail.com>
2019-05-05 22:14:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,24 +1,55 @@
-def roll_call_dwarves(array)
+def roll_call_dwarves(dwarfs)
   index = 0
-  array.each_with_index {|name, index| puts "#{index + 1}. #{name} "}
-end
-
-def summon_captain_planet(array)
-  planeteer_calls = []
-  array.collect do |calls|
-    planeteer_calls << "#{calls.capitalize}!"
+  dwarfs.each_with_index do | name , index |
+    puts "#{index + 1} #{name}"
   end
-  planeteer_calls
 end
 
-def long_planeteer_calls(array)
-  array.any? do |word|
-    word.length > 4
-end
+def summon_captain_planet(planeteer_calls)
+  elements.map!
 end
 
-def find_the_cheese(array)
-  array.find do |cheese|
-    cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
+def long_planeteer_calls# code an argument here
+  # Your code here
+end
+
+def find_the_cheese# code an argument here
+  # the array below is here to help
+  cheese_types = ["cheddar", "gouda", "camembert"]
+end
+
+def roll_call_dwarves(dwarfs)# code an argument here
+  # Your code here
+  #dwarfs = ["Doc", "Dopey", "Bashful", "Grumpy"]
+  dwarfs.each_with_index do | name , index |
+    puts "#{index + 1} #{name}"
+  end
+end
+
+def summon_captain_planet(veggies)# code an argument here
+  # Your code here
+  # planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+    veggies.collect do |call| 
+    call.capitalize + "!"
+  end
+
+end
+
+def long_planteer_calls(long_planteer_calls)# code an argument here
+  # Your code here
+  answer = false
+  long_planteer_calls.each do |call|
+    if call.length > 4
+      answer = true
+    end
+  end
+    answer
+end
+
+def find_the_cheese(cheese)# code an argument here
+  # the array below is here to help
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese.find do |cheese|
+    cheese_types.include?(cheese)
   end
 end
